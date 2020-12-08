95d8c4389fda35ff69b01ef6af1817dab7b72da6
sabago <sandra.abago@gmail.com>
2020-12-08 16:01:31 -0500

    standalone blog

--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,15 @@ import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './store'
 import App from './App.js'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'mdbreact/dist/css/mdb-free.css';
+
+// import '@fortawesome/fontawesome-free/css/all.min.css'; 
+{/* <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"> */}
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+
+// import 'normalize.css'
 
 render(
   <Provider store={store}>
